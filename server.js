d0b10bc5700bc1ac71cf83514fda2810d6199b0e
SimonGPort <simon.giroux.portelance@gmail.com>
2020-06-09 16:59:35 -0400

    solution

---
+++ server.js
@@ -0,0 +1,91 @@
+let express = require("express");
+let app = express();
+
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+app.get(
+  "/http://localhost:8000/api/encrypt/:encrypted/:key",
+  async (req, res) => {
+    let message = req.params.encrypted;
+    let key = req.params.encrypted;
+    message = message.toLowerCase().split("");
+
+    message.map((letter) => {
+      if (letter === " ") {
+        return " ";
+      }
+
+      let index = alphabet.findIndex((alphabetLetter) => {
+        return alphabetLetter === letter;
+      });
+      index = index + key;
+      if (index > 26) {
+        index = 26;
+      }
+      return index;
+    });
+
+    message = message.join("");
+    return res.send(JSON.stringify({ return: message }));
+  }
+);
+
+app.get(
+  "/http://localhost:8000/api/decrypt/:decrypted/:key",
+  async (req, res) => {
+    let message = req.params.decrypted;
+    let key = req.params.encrypted;
+    message = message.toLowerCase().split("");
+
+    message.map((letter) => {
+      if (letter === " ") {
+        return " ";
+      }
+      let index = alphabet.findIndex((alphabetLetter) => {
+        return alphabetLetter === letter;
+      });
+      index = index - key;
+      if (index < 0) {
+        index = 0;
+      }
+      return index;
+    });
+
+    message = message.join("");
+
+    return res.send(JSON.stringify({ return: message }));
+  }
+);
+
+console.log("hello");
+
+app.listen(8000, "0.0.0.0", () => {
+  console.log("app running on port" + 8000);
+});
--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import $ from "jquery";
+import { Button, Form, Alert } from "react-bootstrap";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    // This binding is necessary to make `this` work in the callback
+    this.getUsers = this.getUsers.bind(this);
+    this.state = {
+      firstInput: null,
+      secondInput: null,
+      firstAnswer: null,
+      secondAnswer: null,
+    };
+  }
+
+  getUsers = async (evt) => {
+    evt.preventDefault();
+
+    let response1 = await fetch(
+      `https://reqres.in/api/users/${this.state.firstInput}`
+    );
+    let body1 = await response1.json();
+    body1 = body1.data.first_name;
+
+    let response2 = await fetch(
+      `https://reqres.in/api/users/${this.state.secondInput}`
+    );
+    let body2 = await response2.json();
+    body2 = body2.data.first_name;
+
+    this.setState({ firstAnswer: body1, secondAnswer: body2 });
+  };
+
+  firstInput = (evt) => {
+    this.setState({ firstInput: evt.target.value });
+  };
+
+  secondInput = (evt) => {
+    this.setState({ secondInput: evt.target.value });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="Simple page">
+          <Form onSubmit={this.getUsers}>
+            <Form.Group controlId="formBasicUser1">
+              <Form.Label>User # 1</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="x"
+                onChange={this.firstInput}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicUser2">
+              <Form.Label>User # 2</Form.Label>
+              <Form.Control
+                type="number"
+                placeholder="y"
+                onChange={this.secondInput}
+              />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button type="submit">Get Users info</Button>
+          </Form>
+
+          <Form>
+            <Form.Group controlId="formAnswer">
+              <Form.Text ref="answer">
+                <div>{this.state.firstAnswer}</div>
+                <div>{this.state.secondAnswer}</div>
+                The user data will be written here.
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;
